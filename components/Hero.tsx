63ed55b5660e042b80cff38fa2751e319745c5e0
Anoxity16 <gulshank2612@gmail.com>
2024-09-16 11:31:42 +0530

    Resume Update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,7 +29,6 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          
           <TextGenerateEffect
             words="Transforming Ideas into Seamless User Experiences"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
@@ -41,7 +40,7 @@ const Hero = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1g3tajOxttQX4L2W7NnewkNYHG8OuH2t9/view?usp=sharing"
+            href="https://drive.google.com/file/d/1BFK4ZnocqSA7prW7knDQC9oG-z0sFNLd/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
